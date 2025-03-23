00fc32361b9a73d8368d5a6c1e8206e481a5bf9d
AlexMajumder <alexmajumder55@gmail.com>
2025-03-24 02:43:47 +0600

    update with current position

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,13 +11,98 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Position? position;
+  Position? nowPosition;
+  int number = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    listenCurrentPosition();
+  }
+
+  Future<void> listenCurrentPosition() async {
+    final isGranted = await isLocationPermissionGranted();
+    if (isGranted) {
+      final isServiceEnabled = await checkGPSServiceEnable();
+      if (isServiceEnabled) {
+        Geolocator.getPositionStream(
+          locationSettings: const LocationSettings(
+            distanceFilter: 100,
+            accuracy: LocationAccuracy.bestForNavigation,
+          ),
+        ).listen((pos) {
+          nowPosition = pos;
+          number++;
+          setState(() {});
+        });
+      } else {
+        Geolocator.openLocationSettings();
+      }
+    } else {
+      final result = await requestLocationPermission();
+      if (result) {
+        getCurrentLocation();
+      } else {
+        Geolocator.openAppSettings();
+      }
+    }
+  }
+
+  Future<void> getCurrentLocation() async {
+    final isGranted = await isLocationPermissionGranted();
+    if (isGranted) {
+      final isServiceEnabled = await checkGPSServiceEnable();
+      if (isServiceEnabled) {
+        Position p = await Geolocator.getCurrentPosition();
+        print('Current Position: ${p.latitude}, ${p.longitude}'); // Debug print
+        setState(() {
+          position = p;
+        });
+      } else {
+        Geolocator.openLocationSettings();
+      }
+    } else {
+      final result = await requestLocationPermission();
+      if (result) {
+        getCurrentLocation();
+      } else {
+        Geolocator.openAppSettings();
+      }
+    }
+  }
+
+  Future<bool> isLocationPermissionGranted() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> requestLocationPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> checkGPSServiceEnable() async {
+    return await Geolocator.isLocationServiceEnabled();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Location Guide')),
-      body:  FlutterMap(
+      body: FlutterMap(
         options: MapOptions(
-          initialCenter: LatLng(23.8103, 90.4125),
+          initialCenter: LatLng(nowPosition!.latitude,nowPosition!.longitude), // Default center
           initialZoom: 13.0,
         ),
         children: [
@@ -24,9 +110,21 @@ class _HomeScreenState extends State<HomeScreen> {
             urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             subdomains: ['a', 'b', 'c'],
           ),
+          if (position != null) // Only add MarkerLayer if position is not null
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: LatLng(position!.latitude, position!.longitude),
+                  child: Icon( // Use `child` instead of `builder`
+                    Icons.location_on,
+                    color: Colors.red,
+                    size: 40.0,
+                  ),
+                ),
+              ],
+            ),
         ],
-      )
-      ,
+      ),
     );
   }
 }
